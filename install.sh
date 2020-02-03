bf7a71127e1b463473289abfafd0f225ef4667bc
Ong Boon Leong <elvinongbl@gmail.com>
2020-02-03 20:56:10 +0800

    installer, gitconfig : differentiate installation between work & persnoal
    Make installer differentia between work and personal related environment:-
    
      ./installer.sh personal|work
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# ./install.sh work|personal
+#
+
+if [ x"$1" == x ]; then
+  isPersonal=1
+else
+  isPersonal=0
+fi
+
+if [ x"$1" == xpersonal ]; then
+  isPersonal=1
+fi
+
+if [ x"$1" == xwork ]; then
+  isPersonal=0
+fi
 
 echo -e "Update & upgrade apt repository"
 sudo apt-cache update
@@ -9,13 +26,23 @@ cp ./home/dircolors	$HOME/.dircolors
 echo -e "Environment setup: .vimrc .dircolors"
 
 mkdir -p $HOME/common/bin
-cp ./home/common/bin/socatproxy		$HOME/common/bin/socatproxy
-chmod +x $HOME/common/bin/socatproxy
-echo -e "Tool added: socat for git"
-sudo apt-get install -y socat
 
+echo -e "Environment setup: .gitconfig for work"
 cp ./home/gitconfig	$HOME/.gitconfig
 mkdir -p $HOME/repos/elvinongbl-github
 cp ./home/repos/elvinongbl-github/gitconfig	$HOME/repos/elvinongbl-github/.gitconfig
-echo -e "Environment setup: .gitconfig for work & personal github"
 echo -e "Please add new gmail and patchwork token"
+
+if [ $isPersonal == 0 ]; then
+  echo -e "Further tool chain for proxied work environment"
+  # socat for work proxy
+  cp ./home/common/bin/socatproxy	$HOME/common/bin/socatproxy
+  chmod +x $HOME/common/bin/socatproxy
+  echo -e "Tool added: socat for git"
+  sudo apt-get install -y socat
+else
+  echo -e "Environment setup: ~/.gitconfig for personal"
+  # if environment for personal, overwrite the ~/.gitconfig
+  cp $HOME/repos/elvinongbl-github/.gitconfig	$HOME/.gitconfig
+  rm $HOME/repos/elvinongbl-github/.gitconfig
+fi
