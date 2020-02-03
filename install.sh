95ab154c03d6ab07c3f9287e3e287df11741c13f
Ong Boon Leong <elvinongbl@gmail.com>
2020-02-03 15:07:07 +0800

    socat: initial addition of socat environment for gitproxy
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+echo -e "Update & upgrade apt repository"
+sudo apt-cache update
+sudo apt-get upgrade
+
 cp ./home/vimrc		$HOME/.vimrc
 cp ./home/dircolors	$HOME/.dircolors
 echo -e "Environment setup: .vimrc .dircolors"
+
+mkdir -p $HOME/common/bin
+cp ./home/common/bin/socatproxy		$HOME/common/bin/socatproxy
+chmod +x $HOME/common/bin/socatproxy
+echo -e "Tool added: socat for git"
+sudo apt-get install -y socat
 
 cp ./home/gitconfig	$HOME/.gitconfig
 mkdir -p $HOME/repos/elvinongbl-github
