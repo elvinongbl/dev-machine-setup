8441ec36257fbd3deeb70adaaf82c2cdd1febcd5
Ong Boon Leong <elvinongbl@gmail.com>
2020-02-03 14:55:03 +0800

    gitconfig: add initial version for personal github setting
    It assumes all personal work goes under $HOME/repos/evinongbl-github
    directory.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 cp ./home/vimrc		$HOME/.vimrc
 cp ./home/dircolors	$HOME/.dircolors
-cp ./home/gitconfig	$HOME/.gitconfig
 echo -e "Environment setup: .vimrc .dircolors"
+
+cp ./home/gitconfig	$HOME/.gitconfig
+mkdir -p $HOME/repos/elvinongbl-github
+cp ./home/repos/elvinongbl-github/gitconfig	$HOME/repos/elvinongbl-github/.gitconfig
+echo -e "Environment setup: .gitconfig for work & personal github"
+echo -e "Please add new gmail and patchwork token"
