d830f448429856acc1681ece88b69881a8eb7a6e
Ong Boon Leong <elvinongbl@gmail.com>
2020-04-04 21:44:03 +0800

    install.sh: add vncserver installation and systemd registration
    The steps are from:
    
    https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-18-04
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install.sh
+++ install.sh
@@ -63,3 +63,49 @@ sudo apt-get install -y \
 # Install software packages for system monitoring
 sudo apt-get install -y \
   i7z
+
+# Install vncserver & setup xfce for lightweight vnc session
+sudo apt-get install -y tightvncserver
+sudo apt-get install -y xfce4 xfce4-goodies
+# Trigger vncserver to let your enter password, then kill it
+
+# Back-up existing vnc startup script as we are over-writting
+mv $HOME/.vnc/xstartup $HOME/.vnc/xstartup.bak
+
+# Then, create the vnc startup that start xfce and make it executable
+echo -e "#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &" > $HOME/.vnc/xstartup
+sudo chmod +x $HOME/.vnc/xstartup
+
+# Create vncserver systemd unit
+sudo echo -e "[Unit]
+Description=Start TightVNC server at startup
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=bong5
+Group=bong5
+WorkingDirectory=/home/bong5
+
+PIDFile=/home/bong5/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver -depth 24 -geometry 1670x1000 :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/vncserver@.service
+
+# Reload vncsever systemd unit
+sudo systemctl daemon-reload
+sudo systemctl enable vncserver@1.service
+sudo systemctl start vncserver@1
+
+## Start vncserver manually to enter password
+# $ vncserver
+# $ vncserver -kill :1
+
+## Finally, only the client machine create a ssh pipe to vncserver
+# ssh -L 5901:127.0.0.1:5901 -C -N -l bong5 elvinlatte.local
+# For MAC, open safari, use vnc://localhost:5901
