699eca6f8771833b08bf1616d8b9ef18b4bc086c
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-07 11:18:49 +0800

    network-script: add NETSCRIPT_INSTALL for DUT & LP reference
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install-network-script.sh
+++ install-network-script.sh
@@ -3,6 +3,6 @@
 source ./network-script/setup-global.sh
 
 echo "scp network-script to DUT($DUT_SSH_IPADDR)"
-scp -r network-script/ root@$DUT_SSH_IPADDR:/home/root/
+scp -r network-script/* root@$DUT_SSH_IPADDR:${NETSCRIPT_INSTALL}
 echo "scp network-script to LP($LP_SSH_IPADDR)"
-scp -r network-script/ root@$LP_SSH_IPADDR:/home/root/
+scp -r network-script/* root@$LP_SSH_IPADDR:${NETSCRIPT_INSTALL}
--- network-script/setup-global.sh
+++ network-script/setup-global.sh
@@ -8,14 +8,17 @@ unset LP_B2B_DEVNAME
 unset LP_B2B_IPADDR
 
 # SSH connection
-export DUT_SSH_IPADDR="172.30.181.97"
+export DUT_SSH_IPADDR="172.30.181.93"
 export LP_SSH_IPADDR="172.30.181.80"
 
 # DUT to LP B2B connection
 export DUT_B2B_DEVNAME="enp0s30f4"
-export DUT_B2B_IPADDR="192.168.1.100"
+export DUT_B2B_IPADDR="192.168.1.1"
 export LP_B2B_DEVNAME="enp2s0"
-export LP_B2B_IPADDR="192.168.1.200"
+export LP_B2B_IPADDR="192.168.1.2"
+
+# Target (DUT or LP) installation path
+export NETSCRIPT_INSTALL=/home/root/network-script
 
 alias aplbrd="ssh root@$LP_SSH_IPADDR"
 alias ehlbrd="ssh root@$DUT_SSH_IPADDR"
