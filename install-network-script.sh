611ab827832d6e5a08a75fc9a22889789685a99f
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-07 12:12:29 +0800

    install-network-script: make network-script installation more robust
    We now delete previous version on target (DUT or LP) and install new copy.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- install-network-script.sh
+++ install-network-script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 source ./network-script/setup-global.sh
+source ./network-script/helper-lib.sh
 
 echo "scp network-script to DUT($DUT_SSH_IPADDR)"
+run_dut "rm -rf ${NETSCRIPT_INSTALL}"
+run_dut "mkdir -p ${NETSCRIPT_INSTALL}"
 scp -r network-script/* root@$DUT_SSH_IPADDR:${NETSCRIPT_INSTALL}
+
 echo "scp network-script to LP($LP_SSH_IPADDR)"
+run_lp "rm -rf ${NETSCRIPT_INSTALL}"
+run_lp "mkdir -p ${NETSCRIPT_INSTALL}"
 scp -r network-script/* root@$LP_SSH_IPADDR:${NETSCRIPT_INSTALL}
