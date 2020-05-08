b30326fe5d52651aec630530f7f517a946fc7dbc
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-08 22:36:05 +0800

    network-script: test-01-ping.sh: add full sweep packets swamp test
    For all the packet size below, we send a total of 1024 at 0.01s apart:-
    64B, 128B, 256B, 512B, 800B, 1024B, 1518B, 2048B, 4096B.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/tests/test-01-ping.sh
+++ network-script/tests/test-01-ping.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 #
-# To run a series of ping test for all different speeds
-# as toggled through ethtool
-#
-# Global variables:-
-# LP_B2B_IPADDR - Link Partner's IP address
+# To run a series of ping test for all packet sizes
 
 # include global library
 source ../setup-global.sh
 source ../helper-lib.sh
 
 # Basic Rapid Ping test
-print_banner "Ping 512 packet at 0.01s apart"
-ping -c 512 -i 0.01 $LP_B2B_IPADDR
+print_banner "Ping 1024x 56+8=64Byte packet at 0.01s apart"
+ping -c 1024 -s 56 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 120+8=128Byte packet at 0.01s apart"
+ping -c 1024 -s 120 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 248+8=256Byte packet at 0.01s apart"
+ping -c 1024 -s 248 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 504+8=512Byte packet at 0.01s apart"
+ping -c 1024 -s 504 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 792+8=800Byte packet at 0.01s apart"
+ping -c 1024 -s 792 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 1016+8=1024Byte packet at 0.01s apart"
+ping -c 1024 -s 1016 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 1510+8=1518Byte packet at 0.01s apart"
+ping -c 1024 -s 1510 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 2040+8=2048Byte packet at 0.01s apart"
+ping -c 1024 -s 2040 -i 0.001 $LP_B2B_IPADDR
+
+print_banner "Ping 1024x 4088+8=4096Byte packet at 0.01s apart"
+ping -c 1024 -s 4088 -i 0.001 $LP_B2B_IPADDR
