081e51bf97d1e78c649f0c6d22a3496fb11e09c7
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-09 14:58:29 +0800

    network-script: test-01-ping: enhance test to accept DUT or LP role
    Add test_ping function to better organize test iterations.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/tests/test-01-ping.sh
+++ network-script/tests/test-01-ping.sh
@@ -6,30 +6,27 @@
 source ../setup-global.sh
 source ../helper-lib.sh
 
-# Basic Rapid Ping test
-print_banner "Ping 1024x 56+8=64Byte packet at 0.01s apart"
-ping -c 1024 -s 56 -i 0.001 $LP_B2B_IPADDR
+ROLE=$1
+DEVNAME=$(role2devname $ROLE)
+DEVIPADDR=$(role2ipaddr $ROLE)
+PARTNERIPADDR=$(role2ipaddr $(role2lp $ROLE))
 
-print_banner "Ping 1024x 120+8=128Byte packet at 0.01s apart"
-ping -c 1024 -s 120 -i 0.001 $LP_B2B_IPADDR
+print_banner "Ping test on $ROLE ($DEVIPADDR)"
 
-print_banner "Ping 1024x 248+8=256Byte packet at 0.01s apart"
-ping -c 1024 -s 248 -i 0.001 $LP_B2B_IPADDR
+function test_ping () {
+	# Ping packet size is always +8-Byte
+	SIZE=$[$1-8]
+	COUNT=$2
+	print_topic "Ping - size=$1 [count=$2]"
+	ping -c $COUNT -s $SIZE -i 0.001 $PARTNERIPADDR
+}
 
-print_banner "Ping 1024x 504+8=512Byte packet at 0.01s apart"
-ping -c 1024 -s 504 -i 0.001 $LP_B2B_IPADDR
-
-print_banner "Ping 1024x 792+8=800Byte packet at 0.01s apart"
-ping -c 1024 -s 792 -i 0.001 $LP_B2B_IPADDR
-
-print_banner "Ping 1024x 1016+8=1024Byte packet at 0.01s apart"
-ping -c 1024 -s 1016 -i 0.001 $LP_B2B_IPADDR
-
-print_banner "Ping 1024x 1510+8=1518Byte packet at 0.01s apart"
-ping -c 1024 -s 1510 -i 0.001 $LP_B2B_IPADDR
-
-print_banner "Ping 1024x 2040+8=2048Byte packet at 0.01s apart"
-ping -c 1024 -s 2040 -i 0.001 $LP_B2B_IPADDR
-
-print_banner "Ping 1024x 4088+8=4096Byte packet at 0.01s apart"
-ping -c 1024 -s 4088 -i 0.001 $LP_B2B_IPADDR
+test_ping 64 1024
+test_ping 128 1024
+test_ping 256 1024
+test_ping 512 1024
+test_ping 800 1024
+test_ping 1024 1024
+test_ping 1518 1024
+test_ping 2048 1024
+test_ping 4096 1024
