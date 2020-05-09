add82fa8eab5ebe37917f0ef79fdea199c547db9
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-09 14:27:42 +0800

    network-script: helper-lib: add local scope and role2xxx functions
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/helper-lib.sh
+++ network-script/helper-lib.sh
@@ -9,7 +9,6 @@ function print_separator_short () {
 
 function print_separator () {
 	echo -e "###############################################################"
-	echo -e "\n"
 }
 
 function print_banner () {
@@ -17,11 +16,73 @@ function print_banner () {
 	echo -e "# $1 "
 	echo -e "# Time = $(date)"
 	echo -e "###############################################################"
-	echo -e "\n"
+}
+
+function print_topic () {
+	print_separator_short
+	echo -e "# $1 "
+	print_separator_short
 }
 
 function print_time() {
 	date +"%Y%m%d-%HH%MM"
+}
+
+function print_exe() {
+	echo -e "> $1"
+	$1
+}
+
+function devname2role() {
+	local DEVNAME=$1
+	if [ "$DEVNAME" == "$DUT_B2B_DEVNAME" ]; then
+		echo "DUT"
+		exit 0
+	fi
+	if [ "$DEVNAME" == "$LP_B2B_DEVNAME" ]; then
+		echo "LP"
+		exit 0
+	fi
+	echo "UNKNOWN"
+}
+
+function role2devname() {
+	local ROLE=$1
+	if [ "$ROLE" == "DUT" ]; then
+		echo "$DUT_B2B_DEVNAME"
+		exit 0
+	fi
+	if [ "$ROLE" == "LP" ]; then
+		echo "$LP_B2B_DEVNAME"
+		exit 0
+	fi
+	echo "UNKNOWN"
+}
+
+function role2ipaddr() {
+	local ROLE=$1
+	if [ "$ROLE" == "DUT" ]; then
+		echo "$DUT_B2B_IPADDR"
+		exit 0
+	fi
+	if [ "$ROLE" == "LP" ]; then
+		echo "$LP_B2B_IPADDR"
+		exit 0
+	fi
+	echo "UNKNOWN"
+}
+
+function role2lp() {
+	local ROLE=$1
+	if [ "$ROLE" == "DUT" ]; then
+		echo "LP"
+		exit 0
+	fi
+	if [ "$ROLE" == "LP" ]; then
+		echo "DUT"
+		exit 0
+	fi
+	echo "UNKNOWN"
 }
 
 function run_dut() {
@@ -34,19 +95,19 @@ function run_dut_silence() {
 }
 
 function test_dut() {
-	LOGDIR=$(run_dut_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
-	LOG=$(echo "$1" | sed -e "s#\.sh#\.log#g")
+	local LOGDIR=$(run_dut_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
+	local LOG=$(echo "$1" | sed -e "s#\.sh#\.log#g")
 	echo -e "dut test> $1 [$LOGDIR/$LOG]"
-	ssh root@$DUT_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 > $LOGDIR/$LOG"
+	ssh root@$DUT_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 DUT > $LOGDIR/$LOG"
 }
 
 function getlogs_dut() {
-	LOGDIRPATH=$(run_dut_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
-	LOGDIR=$(echo $LOGDIRPATH | grep -o -P "(?<=${NETSCRIPT_LOG}/).*")
+	local LOGDIRPATH=$(run_dut_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
+	local LOGDIR=$(echo $LOGDIRPATH | grep -o -P "(?<=${NETSCRIPT_LOG}/).*")
 	run_dut_silence "cd $LOGDIRPATH/..; tar -jcvf $LOGDIR.tar.bz2 $LOGDIR"
 	mkdir -p ${TC_LOGS}
 	run_dut_silence "scp -o StrictHostKeyChecking=no $LOGDIRPATH/../$LOGDIR.tar.bz2 ${TC_USER}@${TC_IPADDR}:${TC_LOGS}/"
-	CWD=$(pwd)
+	local CWD=$(pwd)
 	cd ${TC_LOGS}
 	tar xf $LOGDIR.tar.bz2
 	rm $LOGDIR.tar.bz2
@@ -54,8 +115,8 @@ function getlogs_dut() {
 }
 
 function scp2dut() {
-	SRC=$1
-	DST=$2
+	local SRC=$1
+	local DST=$2
 	echo "scp $SRC to $DUT_SSH_IPADDR:$DST"
 	scp $SRC root@$DUT_SSH_IPADDR:$DST
 }
@@ -70,19 +131,21 @@ function run_lp_silence() {
 }
 
 function test_lp() {
-	echo -e "lp test> $1"
-	ssh root@$LP_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1"
+	local LOGDIR=$(run_lp_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
+	local LOG=$(echo "$1" | sed -e "s#\.sh#\.log#g")
+	echo -e "lp test> $1 [$LOGDIR/$LOG]"
+	ssh root@$LP_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 LP > $LOGDIR/$LOG"
 }
 
 function scp2lp() {
-	SRC=$1
-	DST=$2
+	local SRC=$1
+	local DST=$2
 	echo "scp $SRC to $LP_SSH_IPADDR:$DST"
 	scp $SRC root@$LP_SSH_IPADDR:$DST
 }
 
 function setup_test_time() {
-	TIME=$1
+	local TIME=$1
 	run_dut "mkdir -p ${NETSCRIPT_LOG}/$TIME"
 	run_dut "echo "${NETSCRIPT_LOG}/$TIME" > ${NETSCRIPT_INSTALL}/.logpath"
 	run_lp "mkdir -p ${NETSCRIPT_LOG}/$TIME"
@@ -92,8 +155,8 @@ function setup_test_time() {
 # display_eth_info $DEVNAME $COMMENT_STRING
 # e.g. display_eth_info $LP_B2B_DEVNAME "Link Partner"
 function display_eth_info () {
-	DEVNAME=$1
-	COMMENT=$2
+	local DEVNAME=$1
+	local COMMENT=$2
 
 	print_banner "Display Basic Eth Info for $DEVNAME - $COMMENT"
 
@@ -114,12 +177,12 @@ function display_eth_info () {
 }
 
 function remove_all_ipaddr () {
-	DEVNAME=$1
+	local DEVNAME=$1
 	ip address flush dev $DEVNAME
 }
 
 function add_ipaddr () {
-	DEVNAME=$1
-	ADDR=$2
+	local DEVNAME=$1
+	local ADDR=$2
 	ip address add $ADDR/24 dev $DEVNAME
 }
