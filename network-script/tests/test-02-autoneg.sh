22aaedf5df6f35f3014b7533fa894ac6f2c54752
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-11 14:17:00 +0800

    network-script: test-02-autoneg: add link mode and speed default setting
    Everytime there is a change in link mode/speed triggered by AN, we need to
    set the DUT & LP's static IP address.
    
    We add logics to make them default operations when running autoneg speed
    change test.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/tests/test-02-autoneg.sh
+++ network-script/tests/test-02-autoneg.sh
@@ -13,6 +13,14 @@ DEVIPADDR=$(role2ipaddr $ROLE)
 PARTNERIPADDR=$(role2ipaddr $(role2lp $ROLE))
 
 print_banner "Autoneg test on $ROLE ($DEVIPADDR)"
+###
+# Before run autoneg test, make sure LP is full duplex for all speeds
+###
+print_topic "Restore DUT and LP link mode and IP addr"
+run_lp_restore_linkmode $ROLE
+dut_restore_linkmode $ROLE
+run_lp_restore_ipaddr $ROLE
+dut_restore_ipaddr $ROLE
 
 function test_autoneg () {
 	local SPEED=$1
@@ -23,7 +31,10 @@ function test_autoneg () {
 	if [ x"$linkUp" == x"YES" ]; then
 		echo "Link is up at $ROLE"
 		ethtool_show_link $DEVNAME
-		add_ipaddr $DEVNAME $DEVIPADDR
+		# Set IP addr for both DUT & LP
+		run_lp_restore_ipaddr $ROLE
+		dut_restore_ipaddr $ROLE
+		# Ping Test
 		print_separator_short
 		ping -c 10 -s 56 -i 0.001 $PARTNERIPADDR
 	else
@@ -34,3 +45,12 @@ function test_autoneg () {
 test_autoneg 1000 full
 test_autoneg 100 full
 test_autoneg 10 full
+
+###
+# After run autoneg test, restore DUT to both duplex for all speeds
+###
+print_topic "Restore DUT and LP link mode and IP addr"
+run_lp_restore_linkmode $ROLE
+dut_restore_linkmode $ROLE
+run_lp_restore_ipaddr $ROLE
+dut_restore_ipaddr $ROLE
