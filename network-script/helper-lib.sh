083da6cae69b333ff80e3b79a34b9e0b36bbd952
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-11 14:10:35 +0800

    network-script: helper-lib: make ssh, scp, getlogs_lp & speed settings
    The patch involves couple of changes in helper-lib to make the testing
    easier.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/helper-lib.sh
+++ network-script/helper-lib.sh
@@ -33,6 +33,11 @@ function print_exe() {
 	$1
 }
 
+function print_exe_ret() {
+	$1
+	echo -e "> $? = $1"
+}
+
 function devname2role() {
 	local DEVNAME=$1
 	if [ "$DEVNAME" == "$DUT_B2B_DEVNAME" ]; then
@@ -87,18 +92,18 @@ function role2lp() {
 
 function run_dut() {
 	echo -e "dut> $1"
-	ssh root@$DUT_SSH_IPADDR $1
+	ssh -o StrictHostKeyChecking=no root@$DUT_SSH_IPADDR $1
 }
 
 function run_dut_silence() {
-	ssh root@$DUT_SSH_IPADDR $1
+	ssh -o StrictHostKeyChecking=no root@$DUT_SSH_IPADDR $1
 }
 
 function test_dut() {
 	local LOGDIR=$(run_dut_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
-	local LOG=$(echo "$1" | sed -e "s#\.sh#\.log#g")
+	local LOG=DUT-$(echo "$1" | sed -e "s#\.sh#\.log#g")
 	echo -e "dut test> $1 [$LOGDIR/$LOG]"
-	ssh root@$DUT_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 DUT > $LOGDIR/$LOG"
+	ssh -o StrictHostKeyChecking=no root@$DUT_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 DUT > $LOGDIR/$LOG"
 }
 
 function getlogs_dut() {
@@ -118,30 +123,42 @@ function scp2dut() {
 	local SRC=$1
 	local DST=$2
 	echo "scp $SRC to $DUT_SSH_IPADDR:$DST"
-	scp $SRC root@$DUT_SSH_IPADDR:$DST
+	scp -o StrictHostKeyChecking=no $SRC root@$DUT_SSH_IPADDR:$DST
 }
 
 function run_lp() {
 	echo -e "lp> $1"
-	ssh root@$LP_SSH_IPADDR $1
+	ssh -o StrictHostKeyChecking=no root@$LP_SSH_IPADDR $1
 }
 
 function run_lp_silence() {
-	ssh root@$LP_SSH_IPADDR $1
+	ssh -o StrictHostKeyChecking=no root@$LP_SSH_IPADDR $1
 }
 
 function test_lp() {
 	local LOGDIR=$(run_lp_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
-	local LOG=$(echo "$1" | sed -e "s#\.sh#\.log#g")
+	local LOG=LP-$(echo "$1" | sed -e "s#\.sh#\.log#g")
 	echo -e "lp test> $1 [$LOGDIR/$LOG]"
-	ssh root@$LP_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 LP > $LOGDIR/$LOG"
+	ssh -o StrictHostKeyChecking=no root@$LP_SSH_IPADDR "cd $NETSCRIPT_INSTALL/tests; ./$1 LP > $LOGDIR/$LOG"
+}
+
+function getlogs_lp() {
+	local LOGDIRPATH=$(run_lp_silence "cat ${NETSCRIPT_INSTALL}/.logpath")
+	local LOGDIR=$(echo $LOGDIRPATH | grep -o -P "(?<=${NETSCRIPT_LOG}/).*")
+	run_lp_silence "cd $LOGDIRPATH/..; tar -jcvf $LOGDIR.tar.bz2 $LOGDIR"
+	mkdir -p ${TC_LOGS}
+	run_lp_silence "scp -o StrictHostKeyChecking=no $LOGDIRPATH/../$LOGDIR.tar.bz2 ${TC_USER}@${TC_IPADDR}:${TC_LOGS}/"
+	local CWD=$(pwd)
+	cd ${TC_LOGS}
+	tar xf $LOGDIR.tar.bz2
+	rm $LOGDIR.tar.bz2
 }
 
 function scp2lp() {
 	local SRC=$1
 	local DST=$2
 	echo "scp $SRC to $LP_SSH_IPADDR:$DST"
-	scp $SRC root@$LP_SSH_IPADDR:$DST
+	scp -o StrictHostKeyChecking=no $SRC root@$LP_SSH_IPADDR:$DST
 }
 
 function setup_test_time() {
@@ -178,11 +195,43 @@ function display_eth_info () {
 
 function remove_all_ipaddr () {
 	local DEVNAME=$1
-	ip address flush dev $DEVNAME
+	print_exe "ip address flush dev $DEVNAME"
 }
 
 function add_ipaddr () {
 	local DEVNAME=$1
 	local ADDR=$2
-	ip address add $ADDR/24 dev $DEVNAME
+	print_exe "ip address add $ADDR/24 dev $DEVNAME"
+}
+
+function run_lp_restore_ipaddr() {
+	local ROLE=$1
+	if [ x"$ROLE" == x"DUT" ]; then
+		run_lp "source $NETSCRIPT_INSTALL/setup-global.sh; \
+			source $NETSCRIPT_INSTALL/helper-lib.sh; \
+			source $NETSCRIPT_INSTALL/ethtool-lib.sh; \
+			remove_all_ipaddr $LP_B2B_DEVNAME; \
+			add_ipaddr $LP_B2B_DEVNAME $LP_B2B_IPADDR \
+			"
+	else
+		run_dut "source $NETSCRIPT_INSTALL/setup-global.sh; \
+			source $NETSCRIPT_INSTALL/helper-lib.sh; \
+			source $NETSCRIPT_INSTALL/ethtool-lib.sh; \
+			remove_all_ipaddr $DUT_B2B_DEVNAME; \
+			add_ipaddr $DUT_B2B_DEVNAME $DUT_B2B_IPADDR \
+			"
+	fi
+}
+
+function dut_restore_ipaddr() {
+	local ROLE=$1
+	if [ x"$ROLE" == x"LP" ]; then
+		remove_all_ipaddr $LP_B2B_DEVNAME;
+		sleep 1
+		add_ipaddr $LP_B2B_DEVNAME $LP_B2B_IPADDR;
+	else
+		remove_all_ipaddr $DUT_B2B_DEVNAME;
+		sleep 1
+		add_ipaddr $DUT_B2B_DEVNAME $DUT_B2B_IPADDR;
+	fi
 }
