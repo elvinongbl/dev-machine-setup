646817458acee42df4df9862b1c9d72917961cc1
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-11 14:15:26 +0800

    network-script: test-01-ping: add link-mode and speed default setting
    Make test script independent from previous state of test. So, we add
    link mode nd speed default setting to the 1000Mbps Full.
    
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/tests/test-01-ping.sh
+++ network-script/tests/test-01-ping.sh
@@ -5,6 +5,7 @@
 # include global library
 source ../setup-global.sh
 source ../helper-lib.sh
+source ../ethtool-lib.sh
 
 ROLE=$1
 DEVNAME=$(role2devname $ROLE)
@@ -12,6 +13,14 @@ DEVIPADDR=$(role2ipaddr $ROLE)
 PARTNERIPADDR=$(role2ipaddr $(role2lp $ROLE))
 
 print_banner "Ping test on $ROLE ($DEVIPADDR)"
+###
+# Before run autoneg test, make sure LP is full duplex for all speeds
+###
+print_topic "Restore DUT and LP link mode and IP addr"
+run_lp_restore_linkmode $ROLE
+dut_restore_linkmode $ROLE
+run_lp_restore_ipaddr $ROLE
+dut_restore_ipaddr $ROLE
 
 function test_ping () {
 	# Ping packet size is always +8-Byte
@@ -30,3 +39,12 @@ test_ping 1024 1024
 test_ping 1518 1024
 test_ping 2048 1024
 test_ping 4096 1024
+
+###
+# After run autoneg test, restore DUT to both duplex for all speeds
+###
+print_topic "Restore DUT and LP link mode and IP addr"
+run_lp_restore_linkmode $ROLE
+dut_restore_linkmode $ROLE
+run_lp_restore_ipaddr $ROLE
+dut_restore_ipaddr $ROLE
