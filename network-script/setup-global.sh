31ace36ddec4b0d4d7a5d25405a2587fa41455df
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-15 22:54:38 +0800

    TEMP: DUT update
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/setup-global.sh
+++ network-script/setup-global.sh
@@ -8,13 +8,13 @@ unset LP_B2B_DEVNAME
 unset LP_B2B_IPADDR
 
 # SSH connection
-export DUT_SSH_IPADDR="172.30.181.93"
-export LP_SSH_IPADDR="172.30.181.80"
+export DUT_SSH_IPADDR="172.30.181.114"
+export LP_SSH_IPADDR="172.30.181.103"
 
 # DUT to LP B2B connection
-export DUT_B2B_DEVNAME="enp0s30f4"
+export DUT_B2B_DEVNAME="enp0s29f1"
 export DUT_B2B_IPADDR="192.168.1.1"
-export LP_B2B_DEVNAME="enp2s0"
+export LP_B2B_DEVNAME="enp0s29f1"
 export LP_B2B_IPADDR="192.168.1.2"
 
 # Target (DUT or LP) installation path
