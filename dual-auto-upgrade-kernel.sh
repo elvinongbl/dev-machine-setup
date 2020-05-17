e39b6bf935e79f837928a9b92db82cce15462e5d
Ong Boon Leong <elvinongbl@gmail.com>
2020-05-17 14:11:30 +0800

    network-script: dual-auto-upgrade-kernel: add APP|KMOD|ALL option
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- dual-auto-upgrade-kernel.sh
+++ dual-auto-upgrade-kernel.sh
@@ -3,10 +3,19 @@
 source ./network-script/setup-global.sh
 source ./network-script/helper-lib.sh
 
+# Level = "APP", = KMOD" (APP+Kernel Module), "ALL" (APP+Full kernel)
+LEVEL=$1
+
 alive=$(alive_test)
 if [ x"$alive" != x"PASS" ]; then
         echo "ABORT: $alive"
         exit 0
+fi
+
+if [ x"$LEVEL" == x"" ]; then
+	print_topic "Invalid option!" 
+	print_line "Use ./dual-auto-upgrade-kernel.sh APP|KMOD|ALL"
+	exit
 fi
 
 # Delete previous authorized public keys
@@ -16,6 +25,17 @@ rm ${TC_HOME}/.ssh/authorized_keys
 ./install-network-script.sh DUT
 ./install-network-script.sh LP
 
+if [ x"$LEVEL" == x"APP" ]; then
+	print_banner "Upgrade APP Only: DONE!"
+	exit
+fi
+
+# Upgrade kernel modules
+if [ x"$LEVEL" == x"KMOD" ]; then
+	print_banner "Upgrade APP + KMOD: DONE!"
+	exit
+fi
+
 # reload kernel 
 ./rollback-kernel-target.sh DUT
 ./rollback-kernel-target.sh LP 
@@ -23,3 +43,5 @@ rm ${TC_HOME}/.ssh/authorized_keys
 ./upgrade-kernel-target.sh LP  ${UPGRADE_LINUX}
 ./reboot-target.sh DUT
 ./reboot-target.sh LP
+
+print_banner "Upgrade APP + All Kernel: DONE!"
