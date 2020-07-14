45543a8a0fa3e75cb88aae02494f5a88c4d2ceb8
Ong Boon Leong <elvinongbl@gmail.com>
2020-07-14 11:25:05 +0800

    network-scripts: setup-global.sh: add multi platforms definition
    Signed-off-by: Ong Boon Leong <elvinongbl@gmail.com>

--- network-script/setup-global.sh
+++ network-script/setup-global.sh
@@ -7,23 +7,71 @@ unset DUT_B2B_IPADDR
 unset LP_B2B_DEVNAME
 unset LP_B2B_IPADDR
 
-# SSH connection
-export DUT_SSH_IPADDR="172.30.181.101"
-export LP_SSH_IPADDR="172.30.181.93"
-export DUT_SSH_DEVNAME=enp0s20f0u4u4
-export LP_SSH_DEVNAME=enp0s20f0u4u4
+##############################################################
+# Target Platform Selection
+##############################################################
+
+# SSH connection (EHL)
+#export MACHINEARCH=EHL
+#export DUT_SSH_IPADDR="12.30.18.10"
+#export LP_SSH_IPADDR="12.30.18.10"
+#export DUT_SSH_DEVNAME=enp0s20f0u4u4
+#export LP_SSH_DEVNAME=enp0s20f0u4u4
+#export TARGET_STMMAC_IFNAME="enp0s29f1"
+#export TARGET_STMMAC_IFNAME="eth0"
+#export TARGET_IGB_IFNAME="enp1s0"
+
+# SSH connection (TGL)
+export MACHINEARCH=TGL
+export DUT_SSH_IPADDR="12.30.18.11"
+export LP_SSH_IPADDR="12.30.18.11"
+#export TARGET_STMMAC_IFNAME="enp0s30f4"
+export TARGET_STMMAC_IFNAME="eth0"
+export TARGET_IGB_IFNAME="enp1s0"
+
+##############################################################
+# NIC type Info
+##############################################################
+
+# STMMAC Info
+STMMACIFNAME=$TARGET_STMMAC_IFNAME
+STMMAC_SERVER_IP="169.254.1.11"
+STMMAC_CLIENT_IP="169.254.1.22"
+
+#IGB Info
+IGBIFNAME=$TARGET_IGB_IFNAME
+IGB_SERVER_IP="169.254.2.11"
+IGB_CLIENT_IP="169.254.2.22"
+
+##############################################################
+# Network Test NIC Selection
+##############################################################
+
+# Network Test Target Machine & NIC (Integrated TSN)
+TESTIFDEV=$STMMACIFNAME
+TEST_DUT_IPADDR=$STMMAC_SERVER_IP
+TEST_LP_IPADDR=$STMMAC_CLIENT_IP
+
+# Network Test Target Machine & NIC (IGB)
+#TESTIFDEV=$IGBIFNAME
+#TEST_DUT_IPADDR=$IGB_SERVER_IP
+#TEST_LP_IPADDR=$IGB_CLIENT_IP
+
+##############################################################
+# Network Test Common Configuration
+##############################################################
 
 # DUT to LP B2B connection
-export DUT_B2B_DEVNAME="enp0s29f1"
-export DUT_B2B_IPADDR="192.168.1.1"
-export LP_B2B_DEVNAME="enp0s29f1"
-export LP_B2B_IPADDR="192.168.1.2"
+export DUT_B2B_DEVNAME=$TESTIFDEV
+export DUT_B2B_IPADDR=$TEST_DUT_IPADDR
+export LP_B2B_DEVNAME=$TESTIFDEV
+export LP_B2B_IPADDR=$TEST_LP_IPADDR
 
 # Target (DUT or LP) installation path
 export DUT_HOME="/home/root"
 export LP_HOME="/home/root"
-export DUT_REPORT="EHL-A-DUT"
-export LP_REPORT="EHL-B-LP"
+export DUT_REPORT="${MACHINEARCH}-A-DUT"
+export LP_REPORT="${MACHINEARCH}-B-LP"
 export NETSCRIPT_INSTALL=${DUT_HOME}/network-script
 export NETSCRIPT_LOG=${NETSCRIPT_INSTALL}/../test-logs
 
@@ -42,4 +90,11 @@ export TC_PUBKEY="id_rsa.pub"
 alias aplbrd="ssh root@$LP_SSH_IPADDR"
 alias ehlbrd="ssh root@$DUT_SSH_IPADDR"
 
-export UPGRADE_LINUX=${TC_HOME}/work/linux-ese-net/linux-5.4.32-ehl+-x86.tar.xz
+##############################################################
+# Linux Upgrade Information
+##############################################################
+
+export UPGRADE_LINUX_BUILD=${TC_HOME}/work/linux-otc-net
+#export UPGRADE_LINUX=${UPGRADE_LINUX_BUILD}/linux-5.4.34-rt21-ehl+-x86.tar.xz
+export UPGRADE_LINUX=${UPGRADE_LINUX_BUILD}/linux-5.4.49-ehl+-x86.tar.xz
+
